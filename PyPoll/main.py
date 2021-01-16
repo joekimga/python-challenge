c8d02caa9311504f95145b919ef9594aa9333967
Joseph Kim <joekimga@gmail.com>
2021-01-16 08:50:14 -0500

    PyPoll main.py udemy as roadmap

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,6 +18,7 @@ candidates_num = []
 winner = []
 unique_id = []
 
+
 # Open the CSV
 #with open(csvpath) as csvfile:
 with open(file_to_load) as data:
@@ -32,10 +33,11 @@ with open(file_to_load) as data:
 
     total_votes = 0
     candidates = {}
-
+    
     # total votes, candidates, percentage, candidates_num, winner
     #total_votes
     for row in csvreader:
+
         total_votes += 1
         candidate = row[0]
         if candidate in candidates.keys():
@@ -43,7 +45,36 @@ with open(file_to_load) as data:
         else:
             candidates[candidate] = 1
         #print(candidates)
-        print(row)
+        #print(candidate)
+
+    ##########################################
+    ######  UDEMY ROADMAP   #############
+    for row in csvreader:
+        # Count the total number of votes
+        total_votes += 1
+        # Set the candidate names to candidatelist
+        candidates.append(row[2])
+        # Create a set from the candidatelist to get the unique candidate names
+    for x in set(candidates):
+        unique_id.append(x)
+        # y is the total number of votes per candidate
+        y = candidates.count(x)
+        total_votes.append(y)
+        # z is the percent of total votes per candidate
+        z = (y/count)*100
+        vote_percent.append(z)
+
+
+#total_vote  = Tally up the candidates  check
+# if candidate = 1 total_votes
+# total for candidate
+# total for Corry, etc
+# then divide by total_vote
+
+# Do a comparison logic
+# total cand_vote +=
+# divide total by # candidate
+
 
     #Find Candidate First, percentage
 
@@ -53,7 +84,13 @@ with open(file_to_load) as data:
 
 # count candidates,
 
-percentage = total_votes/candidates_num
+# percentage = total_votes/candidates_num
+#percent = round(int(row[6]) / int(row[5]), 2)
+    #review_percent.append(percent)
+percent = round(int(total_votes) / int(candidates_num), 2)
+review_percent.append(percent)
+
+
 print(percentage)
 # winner = Max(total_votes)
 max(candidate)
